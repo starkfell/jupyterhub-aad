2f9d80b81058383a67502eec48f24f26db91a435
Ryan Irujo <ryan.irujo@gmail.com>
2019-08-09 12:46:07 +0200

    Create jupyterhub_config.py

---
+++ jupyterhub_config.py
@@ -0,0 +1,12 @@
+import os
+import jwt
+
+from oauthenticator.azuread import AzureAdOAuthenticator
+c.JupyterHub.authenticator_class = AzureAdOAuthenticator
+
+c.Application.log_level = 'DEBUG'
+
+c.AzureAdOAuthenticator.tenant_id = os.environ.get('AAD_TENANT_ID')
+c.AzureAdOAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL')
+c.AzureAdOAuthenticator.client_id = os.environ.get('APP_CLIENT_ID')
+c.AzureAdOAuthenticator.client_secret = os.environ.get('APP_CLIENT_SECRET')
