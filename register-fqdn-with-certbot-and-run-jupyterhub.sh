819ff9b6146167da5a465361dd8cfda8b34be071
Ryan Irujo <ryan.irujo@gmail.com>
2019-08-13 10:28:38 +0200

    Update and rename register-fqdn-with-certbot.sh to register-fqdn-with-certbot-and-run-jupyterhub.sh

--- register-fqdn-with-certbot-and-run-jupyterhub.sh
+++ register-fqdn-with-certbot-and-run-jupyterhub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Registering the FQDN using certbot. EMAIL_ADDRESS and LETS_ENCRYPT_REGISTER_FQDN are passed in as environment variables.
+echo "--- [Registering the FQDN - $LETS_ENCRYPT_REGISTER_FQDN using certbot] ---"
 certbot \
 certonly \
 --standalone \
@@ -8,3 +9,9 @@ certonly \
 -d $LETS_ENCRYPT_REGISTER_FQDN \
 --agree-tos \
 -n
+
+sleep 5
+
+# Starting Jupyterhub
+echo "--- [Starting Jupyterhub] ---"
+jupyterhub
