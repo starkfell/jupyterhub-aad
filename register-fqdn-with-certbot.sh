e9dcfcf8779d32400df9643043529042512a4b77
Ryan Irujo <ryan.irujo@gmail.com>
2019-08-13 09:44:29 +0200

    Update register-fqdn-with-certbot.sh

--- register-fqdn-with-certbot.sh
+++ register-fqdn-with-certbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Registering the FQDN using certbot.
+# Registering the FQDN using certbot. EMAIL_ADDRESS and LETS_ENCRYPT_REGISTER_FQDN are passed in as environment variables.
 certbot \
 certonly \
 --standalone \
--m EMAIL_ADDRESS \
--d LETS_ENCRYPT_REGISTER_FQDN \
+-m $EMAIL_ADDRESS \
+-d $LETS_ENCRYPT_REGISTER_FQDN \
 --agree-tos \
 -n
