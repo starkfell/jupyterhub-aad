64c3171f0dd270e349e9618c7a213abf1cf6040a
Ryan Irujo <ryan.irujo@gmail.com>
2019-08-13 09:35:51 +0200

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -9,6 +9,11 @@ c.Application.log_level = 'DEBUG'
 c.JupyterHub.hub_port = 8000
 c.JupyterHub.port = 443
 
+c.LetsEncryptRegisterFqdn = os.environ.get('LETS_ENCRYPT_REGISTER_FQDN')
+
+c.JupyterHub.ssl_key = '/etc/letsencrypt/live/{}/privkey.pem'.format(c.LetsEncryptRegisterFqdn)
+c.JupyterHub.ssl_cert = '/etc/letsencrypt/live/{}/fullchain.pem'.format(c.LetsEncryptRegisterFqdn)
+
 c.AzureAdOAuthenticator.tenant_id = os.environ.get('AAD_TENANT_ID')
 c.AzureAdOAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL')
 c.AzureAdOAuthenticator.client_id = os.environ.get('APP_CLIENT_ID')
